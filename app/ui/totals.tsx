2d36b46c58a9302030c3cc317e74a37bedd783e3
Benny Chen <benny.jw.chen@gmail.com>
2025-12-02 21:55:29 -0800

    added rounding

--- app/ui/totals.tsx
+++ app/ui/totals.tsx
@@ -4,10 +4,17 @@ export default function Total({ items }: { items: Item[] }) {
   const total = items.reduce((sum, item) => sum + item.total, 0);
 
   function calculatePaidBy(name: string) {
-    return items
+    const personTotal: number = items
       .filter((item) => item.whoPaid?.toLowerCase() === name.toLowerCase())
       .reduce((sum, item) => sum + item.total, 0);
+    return roundToDecimalPlaces(personTotal, 2);
   }
+
+  function roundToDecimalPlaces(number: number, decimals: number) {
+    const multiple = Math.pow(10, decimals);
+    return Math.round(number * multiple) / multiple;
+  }
+
   return (
     <div>
       <ul>
